7761199c717d1500bb0136a287fdbc4fe1937f36
Fatma Ahmed <113565704+FatmaAhmed25@users.noreply.github.com>
2024-05-17 19:53:33 +0300

    Instructor services

--- coursemanagement/src/main/java/com/example/onlinelearningplatform/controllers/InstructorController.java
+++ coursemanagement/src/main/java/com/example/onlinelearningplatform/controllers/InstructorController.java
@@ -5,8 +5,11 @@ package com.example.onlinelearningplatform.controllers;
 import com.example.onlinelearningplatform.models.Course;
 import com.example.onlinelearningplatform.services.InstructorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/instructor")
@@ -19,6 +22,28 @@ public class InstructorController {
     public ResponseEntity<Object> createCourse(@RequestBody Course course, @RequestParam Long instructorId) {
         return  instructorService.createCourse(course,instructorId);
     }
+
+    @GetMapping("/approvedCourses/{instructorId}")
+    public ResponseEntity<Object> getApprovedCoursesForInstructor(@PathVariable Long instructorId) {
+
+        return instructorService.getApprovedCoursesForInstructor(instructorId);
+    }
+    @GetMapping("/pendingCourses/{instructorId}")
+    public ResponseEntity<Object> getPendingCoursesForInstructor(@PathVariable Long instructorId) {
+
+        return instructorService.getPendingCoursesForInstructor(instructorId);
+    }
+    @GetMapping("/{instructorId}/approved-count")
+    public ResponseEntity<Long> countApprovedCoursesByInstructorId(@PathVariable Long instructorId) {
+        long count = instructorService.countApprovedCoursesByInstructorId(instructorId);
+        return new ResponseEntity<>(count, HttpStatus.OK);
+    }
+    @GetMapping("/{instructorId}/pending-count")
+    public ResponseEntity<Long> countPendingCoursesByInstructorId(@PathVariable Long instructorId) {
+        long count = instructorService.countPendingCoursesByInstructorId(instructorId);
+        return new ResponseEntity<>(count, HttpStatus.OK);
+    }
+
 
 
 
--- coursemanagement/src/main/java/com/example/onlinelearningplatform/services/InstructorService.java
+++ coursemanagement/src/main/java/com/example/onlinelearningplatform/services/InstructorService.java
@@ -2,12 +2,15 @@ package com.example.onlinelearningplatform.services;
 
 
 import com.example.onlinelearningplatform.models.Course;
+import com.example.onlinelearningplatform.models.CourseStatus;
 import com.example.onlinelearningplatform.repositories.CourseRepository;
 import com.example.onlinelearningplatform.repositories.EnrollmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class InstructorService {
@@ -29,5 +32,36 @@ public class InstructorService {
 
         return ResponseEntity.status(HttpStatus.OK).body(savedCourse);
 
+    }
+    public ResponseEntity<Object> getApprovedCoursesForInstructor(Long instructorId) {
+        if(!validationService.validateInstructor(instructorId))
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Instructor with ID: "+instructorId+" doesn't exist");
+        }
+        List<Course> courses = courseRepository.findByInstructorIdAndStatus(instructorId,CourseStatus.APPROVED);
+        if (courses.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(courses);
+    }
+
+    public ResponseEntity<Object> getPendingCoursesForInstructor(Long instructorId) {
+        if(!validationService.validateInstructor(instructorId))
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Instructor with ID: "+instructorId+" doesn't exist");
+        }
+        List<Course> courses = courseRepository.findByInstructorIdAndStatus(instructorId,CourseStatus.PENDING);
+        if (courses.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(courses);
+    }
+    public long countApprovedCoursesByInstructorId(Long instructorId) {
+        return courseRepository.countByInstructorIdAndStatus(instructorId, CourseStatus.APPROVED);
+    }
+    public long countPendingCoursesByInstructorId(Long instructorId) {
+        return courseRepository.countByInstructorIdAndStatus(instructorId, CourseStatus.PENDING);
     }
 }
