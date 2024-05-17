01ff394fb15189b54fa5d99cc20a145c7f961381
sarahyasser <sarahyasserxo@gmail.com>
2024-05-17 21:40:04 +0300

    update

--- frontend/online-learning-platform/src/app/models/Enrollment.model.ts
+++ frontend/online-learning-platform/src/app/models/Enrollment.model.ts
@@ -1,9 +1,10 @@
-// enrollment.model.ts
-export interface Enrollment {
-    id: number;
-    courseId: number;
-    studentId: number;
-    status: string;
-    courseName?: string; // Include courseName if you plan to use it in the UI
-  }
-  
+export interface Enrollment{
+    id:any;
+    studentId:any;
+    courseId:any;
+    status:string;
+    courseName?:string;
+
+
+
+}
